af60689cf534c2193bf6401fc5085da11e04373e
keita <dmwwvn@gmail.com>
2024-01-24 18:06:26 +0900

    🎨 ファイル追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import Header from "./components/header";
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <p className="flex text-blue-700">Hello!!</p>
+      <Header />
+      <Footer />
     </>
   );
 }
