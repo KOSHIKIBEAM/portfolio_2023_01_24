6c0e2c3ebc1268d1ea84b35eba238b83c957b362
keita <dmwwvn@gmail.com>
2024-01-24 18:17:48 +0900

    🎨 Header作成

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
+    <div className="bg-slate-50 text-slate-950 leading-relaxed  antialiased selection:bg-teal-300 selection:text-teal-900 dark:bg-slate-900 dark:text-slate-400">
+      <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 ">
+        <div className="lg:flex lg:justify-between lg:gap-4">
+          <Header />
+          <main className="pt-24 lg:w-1/2 lg:py-24">
+            <Footer />
+          </main>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{jsx,js}"],
-  darkMode: "class",
   theme: {
     extend: {
       animation: {
