8e41dd286d5b8347c5b8e8858819c066aa7dab39
keita <dmwwvn@gmail.com>
2024-01-25 18:30:03 +0900

    🎨 Dark ModeButton 作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from "react";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SkillsSection from "./components/SkillsSection";
 import WorkSection from "./components/WorkSection";
 import AboutSection from "./components/AboutSection";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="bg-slate-50 text-slate-950 leading-relaxed  antialiased selection:bg-teal-300 selection:text-teal-900 dark:bg-slate-900 dark:text-slate-400">
+    <div className="bg-slate-50 text-slate-950 dark:bg-slate-900 dark:text-slate-50 selection:bg-teal-300 selection:text-teal-900">
       <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 ">
         <div className="lg:flex lg:justify-between lg:gap-4">
           <Header />
