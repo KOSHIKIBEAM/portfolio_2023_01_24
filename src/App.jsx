243127a646561edb5ec769cc059074106c7d6999
keita <dmwwvn@gmail.com>
2024-01-26 11:05:49 +0900

    🎨 最初の文字大文字にする

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AboutSection from "./components/AboutSection";
 
 function App() {
   return (
-    <div className="bg-slate-50 text-slate-950 dark:bg-slate-900 dark:text-slate-50 selection:bg-teal-300 selection:text-teal-900">
+    <div className="bg-slate-50 capitalize text-slate-950 dark:bg-slate-900 dark:text-slate-50 selection:bg-teal-300 selection:text-teal-900">
       <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 ">
         <div className="lg:flex lg:justify-between lg:gap-4">
           <Header />
