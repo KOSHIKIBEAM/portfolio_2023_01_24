9e7a1b607bfb75e27679656e198032e318654420
keita <dmwwvn@gmail.com>
2024-01-29 05:25:54 +0900

    🔧 設定ファイル変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,21 @@ export default {
         circleAnim13: "circleAnim 1s forwards 1.25s",
         circleAnim14: "circleAnim 1s forwards 1.35s",
         circleAnim15: "circleAnim 1s forwards 1.45s",
+        circleAnim_sp1: "circleAnim-sp 1s forwards 0s",
+        circleAnim_sp2: "circleAnim-sp 1s forwards .05s",
+        circleAnim_sp3: "circleAnim-sp 1s forwards .15s",
+        circleAnim_sp4: "circleAnim-sp 1s forwards .35s",
+        circleAnim_sp5: "circleAnim-sp 1s forwards .45s",
+        circleAnim_sp6: "circleAnim-sp 1s forwards .55s",
+        circleAnim_sp7: "circleAnim-sp 1s forwards .65s",
+        circleAnim_sp8: "circleAnim-sp 1s forwards .75s",
+        circleAnim_sp9: "circleAnim-sp 1s forwards .85s",
+        circleAnim_sp10: "circleAnim-sp 1s forwards .95s",
+        circleAnim_sp11: "circleAnim-sp 1s forwards 1.05s",
+        circleAnim_sp12: "circleAnim-sp 1s forwards 1.15s",
+        circleAnim_sp13: "circleAnim-sp 1s forwards 1.25s",
+        circleAnim_sp14: "circleAnim-sp 1s forwards 1.35s",
+        circleAnim_sp15: "circleAnim-sp 1s forwards 1.45s",
         bound: "bound 3s infinite",
       },
       keyframes: {
@@ -29,6 +44,14 @@ export default {
           },
           "100%": {
             strokeDasharray: "440 440",
+          },
+        },
+        circleAnim_sp: {
+          "0%": {
+            strokeDasharray: "0 346",
+          },
+          "100%": {
+            strokeDasharray: "346 346",
           },
         },
         bound: {
