fe8737eb0de4e50a160a6b9f4e00fa090bf8746a
keita <dmwwvn@gmail.com>
2024-02-06 18:42:12 +0900

    🎨 レスポンシブ修正

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import AboutSection from "./components/AboutSection";
 function App() {
   return (
     <div className="bg-slate-50 capitalize font-font text-slate-700 dark:bg-blue-950 dark:text-slate-200 selection:text-sky-900 selection:bg-sky-200 dark:selection:bg-teal-300 dark:selection:text-teal-900">
-      <div className="mx-auto min-h-screen max-w-screen-xl 2xl:max-w-screen-2xl px-3 sm:px-6 py-12 md:px-12 md:py-20 lg:px-12 lg:py-0 ">
+      <div className="mx-auto min-h-screen max-w-screen-xl px-3 sm:px-6 py-12 md:px-12 md:py-20 lg:px-12 lg:py-0 ">
         <div className="lg:flex lg:justify-between lg:gap-4 2xl:gap-10 ">
           <Header />
           <main className="lg:pt-24 pt-12 w-full lg:w-1/2 lg:py-24">
