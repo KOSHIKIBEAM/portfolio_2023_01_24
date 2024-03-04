aab2a28776b39b9cd24ac700bda6b23952e76a98
keita <dmwwvn@gmail.com>
2024-03-04 09:28:47 +0900

    🎨

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
           </a>
         </p>
         <h3 className="mt-10 text-xs sm:text-sm font-normal tracking-tight">
-          「なでしこジャパン」パリ五輪出場決定！！
+          日経平均株価史上初の４万円台
         </h3>
       </div>
       <nav className="nav hidden lg:block my-[25px]">
