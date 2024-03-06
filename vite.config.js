e5f7594286e282feca3c0f9fd0682b63827c204f
keita <dmwwvn@gmail.com>
2024-03-06 14:28:42 +0900

    🎨 ファビコン追加

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  link: [
+    { rel: "icon", type: "image/png", href: "/favicon.png" }, // これを追記する
+  ],
+});
