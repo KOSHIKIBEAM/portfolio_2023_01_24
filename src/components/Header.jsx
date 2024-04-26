22071f001f36a10013a9b5c0ffc5d616e4e552aa
keita <dmwwvn@gmail.com>
2024-04-27 06:46:28 +0900

    📝

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -129,7 +129,7 @@ const Header = () => {
               </a>
             </p>
             <h3 className="mt-10 text-xs sm:text-sm font-normal tracking-tight">
-              Ｕ―２３日本、韓国に敗れ２位通過…準々決勝で開催国カタールと対戦
+              Ｕ２３代表、カタールに延長で勝ち４強入り…準決勝に勝てば五輪切符
             </h3>
           </div>
           <nav className="nav hidden lg:block my-[25px]">
