a83e8915a97dd8361fcd4a63fbd7fd5600525a9c
keita <dmwwvn@gmail.com>
2024-04-30 05:08:03 +0900

    📝

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -129,7 +129,7 @@ const Header = () => {
               </a>
             </p>
             <h3 className="mt-10 text-xs sm:text-sm font-normal tracking-tight">
-              大谷翔平に毎打席の激しいブーイング…ドジャースの連勝６でストップ
+              サッカーＵ２３日本代表、パリ五輪出場決定…イラクに２―０で勝利
             </h3>
           </div>
           <nav className="nav hidden lg:block my-[25px]">
