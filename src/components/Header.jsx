60a22c0673e27dd258dcc109842b899cb2231964
keita <dmwwvn@gmail.com>
2024-06-26 11:09:19 +0900

    📝

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
               </a>
             </p>
             <h3 className="mt-10 text-xs sm:text-sm font-normal tracking-tight">
-              ヤクルト宅配スタッフが不用品回収、メルカリ出品…広島県の２市で実証実験
+              ブックオフの複数店舗で架空買い取りなど不適切事案か、臨時休業発生の見込み
             </h3>
           </div>
           <nav className="nav hidden lg:block my-[25px]">
@@ -129,7 +129,7 @@ const Header = () => {
               </a>
             </p>
             <h3 className="mt-10 text-xs sm:text-sm font-normal tracking-tight">
-              ヤクルト宅配スタッフが不用品回収、メルカリ出品…広島県の２市で実証実験　
+              ブックオフの複数店舗で架空買い取りなど不適切事案か、臨時休業発生の見込み　
             </h3>
           </div>
           <nav className="nav hidden lg:block my-[25px]">
