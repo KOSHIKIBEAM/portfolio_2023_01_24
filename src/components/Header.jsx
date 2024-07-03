e13e63208968b6e4ad8e457bff0b782f457a4882
keita <dmwwvn@gmail.com>
2024-07-03 13:17:52 +0900

    📝

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
               </a>
             </p>
             <h3 className="mt-10 text-xs sm:text-sm font-normal tracking-tight">
-              千葉県ＰＴＡ協議会、全国組織を年度末に退会へ…「不透明な会計処理あり苦渋の決断」
+              渋沢・津田・北里きょう発行　旧紙幣「交換」詐欺ご用心
             </h3>
           </div>
           <nav className="nav hidden lg:block my-[25px]">
@@ -129,7 +129,7 @@ const Header = () => {
               </a>
             </p>
             <h3 className="mt-10 text-xs sm:text-sm font-normal tracking-tight">
-              千葉県ＰＴＡ協議会、全国組織を年度末に退会へ…「不透明な会計処理あり苦渋の決断」　
+              渋沢・津田・北里きょう発行　旧紙幣「交換」詐欺ご用心　
             </h3>
           </div>
           <nav className="nav hidden lg:block my-[25px]">
